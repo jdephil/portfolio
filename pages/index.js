98ed783f96a14dab1e5635da19ce5f49dd042f6b
jdephil <jennifer.dephillips@gmail.com>
2020-08-10 17:27:19 -0400

    finished header css

--- pages/index.js
+++ pages/index.js
@@ -9,21 +9,46 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className='main'>
-        <div>
-          <img src="/forest.jpg" alt='mountains and forest with sunrise' />
+      <main className={styles.main}>
+        <div className={styles.headerDiv}>
+        <img className={styles.forestImg} src="/sunrise.jpg" />
+          <div className={styles.jenDiv}>
+            <h1 className={styles.h1}>I'm Jennifer</h1>
+          </div>
+          <div className={styles.descDiv}>
+            <h2 className={styles.h2}>a front end developer <br></br> creating online experiences <br></br> using CSS, JavaScript, and React</h2>
+          </div>
+          <div className={styles.sunDiv}>
+            <img className={styles.sun} src="/sun1.png" alt="sun" />
+          </div>
+         <div className={styles.attribution}><a href='https://www.freepik.com/vectors/background'>Background vector created by freepik - www.freepik.com</a></div>
+        
+        
+        </div>
+
+        <div className={styles.aboutDiv}>
+          <p> 
+            I use my passion for building projects and telling stories to design cohesive websites that truly represent the brands they are created for. 
+          </p>
+        </div>
+        <div className={styles.campingDiv}>
+          <h3>Camping Trip</h3>
+          <p>Search the sky and find your favorite celestial bodies using this app and the NASA Images API</p>
+        </div>
+        
+        <div className={styles.wonderDiv}>
+          <h3>World of Wonder</h3>
+          <p>Escape for the day into our virtual museum, exploring different artifacts and cultures. Take notes in your journal and curate a collection of your own.</p>
+        </div>
+        
+        <div className={styles.frogDiv}>
+          <h3>Frog Story</h3>
+          <p>A little RPG choose-your-own-adventure about a lonely frog.</p>
         </div>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+          Twitter LinkedIn GitHub
       </footer>
     </div>
   )
