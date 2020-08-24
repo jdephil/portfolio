3b2a8ecce1b349429580283e52ce158e5acfc072
jdephil <jennifer.dephillips@gmail.com>
2020-08-24 18:07:05 -0400

    footer links styled

--- components/footer.js
+++ components/footer.js
@@ -1,12 +1,20 @@
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Footer.module.css'
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-        <a href="https://www.twitter.com/JenDePhillips" target="_blank">Twitter</a>
-        <a href="https://www.linkedin.com/in/jenniferpaigedephillips/" target="_blank">LinkedIn</a>
-        <a href="https://github.com/jdephil" target="_blank">GitHub</a>
-        <a className={styles.hover} href='/resume.pdf' download>Resume</a>
+      <div className={styles.linkDiv}>
+        <a href="https://www.twitter.com/JenDePhillips" target="_blank"><p className={styles.a}>Twitter</p></a>
+      </div>
+      <div className={styles.linkDiv}>
+        <a href="https://www.linkedin.com/in/jenniferpaigedephillips/" target="_blank"><p className={styles.a}>LinkedIn</p></a>
+      </div>
+      <div className={styles.linkDiv}>
+        <a href="https://github.com/jdephil" target="_blank"><p className={styles.a}>GitHub</p></a>
+      </div>
+      <div className={styles.linkDiv}>
+        <a className={styles.hover} href='/resume.pdf' download><p className={styles.a}>Resume</p></a>
+      </div>
         <p className={styles.copyright}> ©️ Jennifer De Phillips 2020</p>
     </footer>
   )
