498d110308c6382e93c81d6b3c1643cae68dd80f
jdephil <jennifer.dephillips@gmail.com>
2020-08-24 13:43:23 -0400

    added screenshots and link effects

--- components/middle.js
+++ components/middle.js
@@ -3,26 +3,43 @@ import styles from '../styles/Home.module.css'
 export default function Middle() {
   return (
     <div className={styles.middle}>
-      <img className={styles.purpDiamonds} src="/purpleDiamonds.png" alt="purple diamonds" />
-      <img className={styles.blackDiamonds} src="/blackDiamonds.png" alt="black diamonds" />
+      <div className={styles.pdDiv}>
+        <img className={styles.purpDiamonds} src="/purpleDiamonds.png" alt="purple diamonds" />
+      </div>
+      {/* <div className={styles.bdDiv}>
+        <img className={styles.blackDiamonds} src="/blackDiamonds.png" alt="black diamonds" />
+      </div> */}
       <div className={styles.aboutDiv}>
         <p> 
           I use my passion for telling stories and building projects to design cohesive websites that truly represent the brands they are created for. 
         </p>
       </div>
+      <div className={styles.campingImgDiv}>
+        <img className={styles.campingImg} src='/projects/campingTrip.png' alt='camping trip project' />
+      </div>
       <div className={styles.campingDiv}>
-        <h3>Camping Trip</h3>
-        <p>Search the sky and find your favorite celestial bodies using this app and the NASA Images API.</p>
+        <div className={styles.linkDiv}>
+          <a href='https://campingtrip-starsearch.herokuapp.com/' ><h3 className={styles.a}>Camping Trip</h3></a>
+        </div>
+        <p className={styles.p}>Search the sky and find your favorite celestial bodies using this app and the NASA Images API. Created with Express, Sequelize, Postgres, EJS, and styled with CSS.</p>
       </div>
-      
+      <div className={styles.wowImgDiv}>
+        <img className={styles.wowImg} src='/projects/wow.png' alt='world of wonder project' />
+      </div>
       <div className={styles.wonderDiv}>
-        <h3>World of Wonder</h3>
-        <p>Escape for the day into our virtual museum, exploring different artifacts and cultures. Take notes in your journal and curate a collection of your own.</p>
+        <div className={styles.linkDiv}>
+         <a href='https://loving-williams-21d7ec.netlify.app/' ><h3 className={styles.a}>World of Wonder</h3></a>
+        </div>
+        <p>Escape for the day into our virtual museum, exploring different artifacts and cultures. Take notes in your journal and curate a collection of your own. A group project of 3 other developers, created with React and MongoDB, styled with Materialize and CSS.</p>
       </div>
-      
+      <div className={styles.frogImgDiv}>
+        <img className={styles.frogImg} src='/projects/frogStory.png' alt='frog story' />
+      </div>
       <div className={styles.frogDiv}>
-        <h3>Frog Story</h3>
-        <p>A choose-your-own-adventure game about a lonely, little frog.</p>
+        <div className={styles.linkDiv}>
+         <a href='https://jdephil.github.io/Frog-Story/' ><h3 className={styles.a}>Frog Story</h3></a>
+        </div>
+        <p>A choose-your-own-adventure game about a lonely, little frog. Created with vanilla Javascript, CSS, and HTML.</p>
       </div>
       <div className={styles.profDiv}>
         <p className={styles.connectP}>I would love to help you merge good design with functional code. Connect with me on one of the platforms below!</p>
